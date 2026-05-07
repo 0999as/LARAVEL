4ded41183081b2ef38768d696c40e9f989da5649
0999as <i@example.com>
2026-05-07 04:33:53 -0700

    save

---
+++ app/Http/SaveController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User10;
+use App\Models\Profiles;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class HasController extends Controller
+{
+    public function store(Request $request) {
+        DB::begineTransaction();
+        try {
+            
+        }
+    }
+}
